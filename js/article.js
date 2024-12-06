b536495d47ae01c87d0a05cedfc35ec3ac679b9a
Antonio Ortiz <ortiz.antonio@gmail.com>
2024-12-06 02:29:49 -0600

    Updates

--- js/article.js
+++ js/article.js
@@ -1,5 +1,14 @@
+const socialShareMenu = document.getElementById('social-share');
+const articleShare = document.getElementById("articleShare");
+const menuTitle = document.getElementById("share");
+
+let isMenuExpanded = false;
+
+menuClose();
+
 const buttonShare = `
     <button
+      class="article__share--button"
       aria-label="open share menu"
       aria-expanded="false"
       aria-controls="social-share"
@@ -9,6 +18,39 @@ const buttonShare = `
     </button>
 `;
 
-const articleShare = document.getElementById("articleShare");
-
 articleShare.insertAdjacentHTML('beforeend', buttonShare);
+
+articleShare.addEventListener('click', function (e) {
+  const button = e.target.closest('.article__share--button');
+  if (button) {
+    toggleMenu(button);
+  }  
+});
+
+function toggleMenu(button){
+  isMenuExpanded = !isMenuExpanded;
+
+  if(button){
+    button.setAttribute('aria-expanded', isMenuExpanded);
+  }
+  
+  if(isMenuExpanded){
+    menuOpen();
+  }
+  if(!isMenuExpanded){
+    menuClose();
+  }
+}
+
+function menuOpen(){
+  socialShareMenu.style.display = "block";
+  menuTitle.style.display = "block";
+  /*   socialShareMenu.setAttribute('aria-hidden', "false"); */
+  /*   socialShareMenu.firstElementChild.querySelector('a').focus(); */
+}
+
+function menuClose(){
+  socialShareMenu.style.display = "none";
+  menuTitle.style.display = "none";
+  /*   socialShareMenu.setAttribute('aria-hidden', "true"); */
+}
