5c32831c10e9342fafec602025a1c3cc9854aa60
Antonio Ortiz <ortiz.antonio@gmail.com>
2024-12-16 18:37:13 -0600

    Updates

--- js/article.js
+++ js/article.js
@@ -1,4 +1,3 @@
-"use strict";
 // Get DOM elements with type safety
 const socialShareMenu = document.getElementById('articleShare');
 const articleFooter = document.getElementById('articleFooter');
@@ -16,29 +15,28 @@ const buttonShare = `
     </button>
 `;
 if (articleFooter) {
-    articleFooter.insertAdjacentHTML('beforeend', buttonShare);
-    articleFooter.addEventListener('click', (e) => {
-        const target = e.target;
-        const button = target?.closest('.article__footer--share');
-        if (button) {
-            toggleMenu(button);
-        }
-    });
+  articleFooter.insertAdjacentHTML('beforeend', buttonShare);
+  articleFooter.addEventListener('click', (e) => {
+    const target = e.target;
+    const button = target?.closest('.article__footer--share');
+    if (button) {
+      toggleMenu(button);
+    }
+  });
 }
 function toggleMenu(button) {
-    isMenuExpanded = !isMenuExpanded;
-    button.setAttribute('aria-expanded', isMenuExpanded.toString());
-    setDisplay();
+  isMenuExpanded = !isMenuExpanded;
+  button.setAttribute('aria-expanded', isMenuExpanded.toString());
+  setDisplay();
 }
 function setDisplay() {
-    let display = '';
-    if (isMenuExpanded) {
-        display = 'flex';
-    }
-    else {
-        display = 'none';
-    }
-    if (socialShareMenu) {
-        socialShareMenu.style.display = display;
-    }
+  let display = '';
+  if (isMenuExpanded) {
+    display = 'flex';
+  } else {
+    display = 'none';
+  }
+  if (socialShareMenu) {
+    socialShareMenu.style.display = display;
+  }
 }
